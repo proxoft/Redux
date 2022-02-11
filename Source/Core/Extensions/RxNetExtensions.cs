3c842554988daf0e35ca896c8be7eae9f5fdb313
Michal Strbka <Michal.Strbka@zuehlke.com>
2022-02-11 13:52:26 +0100

    Extension methods SelectVoid, WhereStateAction, etc.

---
+++ Source/Core/Extensions/RxNetExtensions.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Reactive;
+using System.Reactive.Linq;
+
+namespace Proxoft.Redux.Core.Extensions
+{
+    public static class RxNetExtensions
+    {
+        public static IObservable<Unit> SelectVoid<TS>(this IObservable<TS> source)
+        {
+            return source
+                .Select(_ => Unit.Default);
+        }
+
+        public static IObservable<TR> SelectDistinctUntilChanged<T, TR>(
+            this IObservable<T> source,
+            Func<T, TR> selector
+        )
+        {
+            return source
+                .Select(selector)
+                .DistinctUntilChanged();
+        }
+
+        public static IObservable<TR> SelectDistinctUntilChanged<T, TR, TKey>(
+            this IObservable<T> source,
+            Func<T, TR> selector,
+            Func<TR, TKey> keySelector
+        )
+        {
+            return source
+                .Select(selector)
+                .DistinctUntilChanged(keySelector);
+        }
+
+        public static IObservable<T> OfTypeWhere<T>(
+            this IObservable<T> source,
+            Func<T, bool> predicate)
+            where T : class
+        {
+            return source
+                .OfType<T>()
+                .Where(i => predicate(i));
+        }
+    }
+}
---
+++ Source/Core/Extensions/StoreExtensions.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Reactive;
+using System.Reactive.Linq;
+
+namespace Proxoft.Redux.Core.Extensions
+{
+    public static class StoreExtensions
+    {
+        public static IObservable<(TS, TA)> WhereStateAction<TS, TA>(
+            this IObservable<StateActionPair<TS>> source,
+            Func<TS, bool> statePredicate)
+            where TA : IAction
+        {
+            return source.
+                WhereStateAction<TS, TA>(statePredicate, _ => true);
+        }
+
+        public static IObservable<(TS, TA)> WhereStateAction<TS, TA>(
+            this IObservable<StateActionPair<TS>> source,
+            Func<TA, bool> actionPredicate)
+            where TA : IAction
+        {
+            return source.
+                WhereStateAction(_ => true, actionPredicate);
+        }
+
+        public static IObservable<(TS, TA)> WhereStateAction<TS, TA>(
+            this IObservable<StateActionPair<TS>> source,
+            Func<TS, bool> statePredicate,
+            Func<TA, bool> actionPredicate)
+            where  TA : IAction
+        {
+            return source
+                .Where(pair => (pair.Action is TA taAction)
+                            && actionPredicate(taAction)
+                            && statePredicate(pair.State))
+                .Select(pair => (pair.State, (TA)pair.Action));
+        }
+
+        public static IObservable<TS> SelectState<TS>(this IObservable<StateActionPair<TS>> source)
+        {
+            return source
+                .Select(pair => pair.State);
+        }
+
+        public static IObservable<IAction> SelectAction<TS>(this IObservable<StateActionPair<TS>> source)
+        {
+            return source
+                .Select(pair => pair.Action);
+        }
+
+        public static IObservable<TS> SelectState<TS, TA>(this IObservable<(TS state, TA action)> source)
+            where TA : IAction
+        {
+            return source
+                .Select(pair => pair.state);
+        }
+
+        public static IObservable<TA> SelectAction<TS, TA>(this IObservable<(TS state, TA action)> source)
+            where TA : IAction
+        {
+            return source
+                .Select(pair => pair.action);
+        }
+    }
+}
