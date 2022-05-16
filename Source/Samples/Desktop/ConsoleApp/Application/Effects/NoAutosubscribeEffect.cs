c6305fdddd6782e23d2d49320c7676ec25e6ccdb
drvic10k <matus.brlit@gmail.com>
2022-05-16 17:33:58 +0200

    rename demo effect

--- Source/Samples/Desktop/ConsoleApp/Application/Effects/NoAutosubscribeEffect.cs
+++ Source/Samples/Desktop/ConsoleApp/Application/Effects/NoAutosubscribeEffect.cs
@@ -1,23 +1,19 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Reactive.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using ConsoleApp.Application.Actions;
 using Proxoft.Redux.Core;
 using Proxoft.Redux.Core.Actions;
 
 namespace ConsoleApp.Application.Effects
 {
-    public class NoAutosubscribeEffect : Effect<ApplicationState>
+    public class NoAutoSubscribeEffect : Effect<ApplicationState>
     {
         [IgnoreSubscribe]
         private IObservable<IAction> ShouldNotDispatch => this.ActionStream
             .Do(action => Console.WriteLine("!!!!this line should be executed!!!"))
             .Select(action => action switch
             {
-                InitializeEffectsAction _ => new SetMessageAction("this action should not be dispatched", nameof(NoAutosubscribeEffect)),
+                InitializeEffectsAction _ => new SetMessageAction("this action should not be dispatched", nameof(NoAutoSubscribeEffect)),
                 _ => DefaultActions.None
             })
             .Where(action => action != DefaultActions.None);
