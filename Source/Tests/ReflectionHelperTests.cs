1d253bf848495cdbadbe8237ba61763486f32a09
drvic10k <matus.brlit@gmail.com>
2022-05-17 14:17:05 +0200

    fixed subscribing when class contains generic methods; added unit tests

---
+++ Source/Tests/ReflectionHelperTests.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Linq;
+using System.Reactive;
+using System.Reactive.Subjects;
+using Proxoft.Redux.Core;
+using Proxoft.Redux.Core.Effects;
+using Xunit;
+
+namespace Tests
+{
+    public class ReflectionHelperTests
+    {
+        [Fact]
+        public void GetObservableProperties_AutoSubscribe()
+        {
+            Assert.Single(new TestClass().GetObservableProperties<IAction>(false));
+            Assert.Empty(new TestClass().GetObservableProperties<Unit>(false));
+            Assert.Equal(2, new TestClass().GetObservableProperties<object>(false).Count());
+        }
+
+        [Fact]
+        public void GetObservableMethods_AutoSubscribe()
+        {
+            Assert.Single(new TestClass().GetObservableMethods<IAction>(false));
+            Assert.Empty(new TestClass().GetObservableMethods<Unit>(false));
+            Assert.Equal(2, new TestClass().GetObservableMethods<object>(false).Count());
+        }
+
+        [Fact]
+        public void GetObservableProperties_ManualSubscribe()
+        {
+            Assert.Single(new TestClass().GetObservableProperties<IAction>(true));
+            Assert.Empty(new TestClass().GetObservableProperties<Unit>(true));
+            Assert.Single(new TestClass().GetObservableProperties<object>(true));
+        }
+
+        [Fact]
+        public void GetObservableMethods_ManualSubscribe()
+        {
+            Assert.Single(new TestClass().GetObservableMethods<IAction>(true));
+            Assert.Empty(new TestClass().GetObservableMethods<Unit>(true));
+            Assert.Single(new TestClass().GetObservableMethods<object>(true));
+        }
+
+        private class TestClass
+        {
+            [Subscribe]
+            private IObservable<IAction> Property1 => new Subject<IAction>();
+
+            private IObservable<string> Property2 => new Subject<string>();
+
+            [IgnoreSubscribe]
+            private IObservable<IAction> Property3 => new Subject<IAction>();
+
+            [Subscribe]
+            private IObservable<IAction> Method1() => new Subject<IAction>();
+
+            private IObservable<string> Method2() => new Subject<string>();
+
+            [IgnoreSubscribe]
+            private IObservable<IAction> Method3() => new Subject<IAction>();
+
+            private IObservable<TAction> GenericMethod<TAction>() where TAction : IAction
+            {
+                return new Subject<TAction>();
+            }
+        }
+    }
+}
