d24a839a836d09fe79d76ccc026d85df8bf7bf4a
Michal Strbka <Michal.Strbka@zuehlke.com>
2023-05-25 15:42:32 +0200

    cleanup

--- Source/Samples/Blazor/BlazorApp/Client/ActionJournaler.cs
+++ Source/Samples/Blazor/BlazorApp/Client/ActionJournaler.cs
@@ -1,10 +1,11 @@
-﻿using Proxoft.Redux.Core;
+﻿using System;
+using Proxoft.Redux.Core;
 
 namespace BlazorApp.Client
 {
     public class ActionJournaler : IActionJournaler
     {
-        public void Journal(IAction action)
+        public void Journal(IAction action, Type? sender = null)
         {
             System.Console.WriteLine($"dispatched: {action.GetType().Name}");
         }
